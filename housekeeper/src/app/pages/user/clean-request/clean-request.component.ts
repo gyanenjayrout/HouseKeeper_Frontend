2f38f32568136468e7a4a11c00a5b54e10c64322
Gyanenjay Rout <88276612+gyanenjayrout@users.noreply.github.com>
2023-08-11 16:06:21 +0530

    clean request service sent

--- housekeeper/src/app/pages/user/clean-request/clean-request.component.ts
+++ housekeeper/src/app/pages/user/clean-request/clean-request.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserRequestService } from 'src/app/services/user-request.service';
+import swal from 'sweetalert';
 @Component({
   selector: 'app-clean-request',
   templateUrl: './clean-request.component.html',
   styleUrls: ['./clean-request.component.css']
 })
-export class CleanRequestComponent {
+export class CleanRequestComponent implements OnInit {
+
+
+  public request ={
+    
+    date: '',
+   cleaningtime: '',  // Using string for time, you can use a custom pipe for formatting
+  roomno: '',
+   floorno: '',
+  hostelname: '',
+
+
+  }
+  ngOnInit(): void {
+    
+  }
+
+  constructor(private sendreq:UserRequestService, private snack:MatSnackBar)
+  {
+
+  }
+
+reqSubmit()
+{
+  // if(this.request.cleaningtime === '' || this.request.cleaningtime === null||this.request.roomno==""||this.request.roomno==null)
+  // {
+  //   this.snack.open('cleaningtime is required','',
+  //     {
+  //       duration:3000,
+  //       // verticalPosition:'top',
+  //       // horizontalPosition:'center',
+  //     })
+  //     return;
+  // }
+  this.sendreq.sendRequest(this.request).subscribe({
+    next: (data: any) => {
+      console.log(data);
+      swal('User '+data.username +' Request Submitted')
+    },
+    error: (error: any) => {
+      console.log(error);
+      
+      this.snack.open('Server Error','',
+    {
+      duration:3000,
+      
+    })
+    },
+    
+  });
+
+
+}
+
 
 }
--- housekeeper/src/app/services/user-request.service.ts
+++ housekeeper/src/app/services/user-request.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+const baseUrl = 'http://localhost:7081';
 @Injectable({
   providedIn: 'root'
 })
 export class UserRequestService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  public sendRequest(req:any)
+  {
+    return this.http.post(`${baseUrl}/cleanRequest/saveCleanRequest`,req);
+
+  }
 }
